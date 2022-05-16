a933074b879c73ba3d0041574be85d76c6a19dde
elementtest <elementtest7@gmail.com>
2022-05-16 13:43:31 +0800

    did it again

--- myscripts.js
+++ myscripts.js
@@ -32,26 +32,51 @@ function playerPlay(playerChance) {
 playerSelection = playerPlay(prompt('what do you throw?').toLowerCase());
 
 console.log('player selection is ' + playerSelection);
-
+var ccScore = 0;
+var ppScore = 0;
 function fFinal(playerSelection, computerSelection) {
+
     if (playerSelection === 'rock' && computerSelection === 'paper') {
         console.log('Computer wins');
+        ccScore += 1;
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
         console.log('Player wins');
+        ppScore += 1;
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
         console.log('player wins');
+        ppScore += 1;
     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
         console.log('computer wins');
+        ccScore += 1;
     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
         console.log('computer wins');
+        ccScore += 1;
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
         console.log('player wins');
+        ppScore += 1;
     } else if (playerSelection == computerSelection) {
         console.log('it is a tie');
     } else {
         console.log('something went wrong');
     }
-
 }
 
 fFinal(playerSelection, computerSelection);
+console.log('begin for loop');
+for (i=0; i<5; i++) {
+    fFinal(playerSelection, computerSelection);
+    computerSelection = computerPlay();
+    console.log('you already played::::' + playerSelection);
+    console.log('computer plays ' + computerSelection);
+    
+};
+
+console.log('comp score is ' + ccScore);
+console.log('player score is ' + ppScore);
+if (ccScore > ppScore) {
+    console.log('computer wins overall');
+} else if (ppScore > ccScore) {
+    console.log('the player defeats the terminator');
+} else {
+    console.log('no winner this time');
+}
